c8aa247e55471c16a4481cba008d092424e8bccd
Tahsin Amin <Tahsin.Amin@edu.sait.ca>
2024-04-16 13:47:39 -0600

    Complete with Bugs

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,10 +6,19 @@ import Image from 'next/image'
 import Header from "./Header";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
-
+import Users from '../models/users'
+import { connectToMongoDB } from '../lib/mongodb'
+import { useState } from "react"
 
 const Home = () => {
+  const [addAdminUser, setAddAdminUser] = useState('');
   const { status, data: session } = useSession();
+  connectToMongoDB();
+  const user = Users.findOne({name: session.user.name});
+
+  const addAdmin = async () => {
+    await Users.updateOne({name: addAdminUser}, {admin: true});
+  }
 
   return status === "authenticated" ?
   (
@@ -36,6 +45,26 @@ const Home = () => {
 
         </div>
       </div>
+
+      { user.admin ? (
+
+      <div className='mt-16 flex flex-row justify-between items-center'>
+        <h6 className='font-semibold text-white text-sm'> Make a user Admin/Not Admin: </h6>
+      
+        <input
+        type='text'
+        placeholder='User email'
+        className='w-1/2 font-semibold rounded-md p-2'
+        onChange={(e) => setAddAdminUser(e.target.value)}
+        />
+
+        <button className='bg-red-700 hover:bg-red-900 p-2 mx-2'
+        onClick={addAdmin}> Save </button>
+      </div>
+
+      ) : (
+        <div className='text-white text-sm font-semibold'> You are logged in as: {user.name} </div>
+      )}
 
       <Footer/>
     </div>
